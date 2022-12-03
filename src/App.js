0fbfe39d282bda848f7453415268ab5337c739ef
sheebukhan62 <sheebukhan62@gmail.com>
2022-12-03 14:55:49 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+import {motion} from 'framer-motion';
+import {useRef, useEffect, useState} from 'react'
+import images from './Images';
+
 
 function App() {
+
+  const [width, setWidth] = useState(0);
+  const carousel = useRef();
+
+  useEffect(() => {
+    console.log(carousel.current.scrollWidth, carousel.current.offsetWidth)
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth)
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Image Drag Sider</h1>
+      <motion.div ref={carousel} className="carousel" whileTap={{cursor:'grabbing'}}>
+        <motion.div 
+        drag="x" 
+        dragConstraints={{right:0, left: -width}} 
+        className="inner-carousel">
+          {images.map(image => {
+            return(
+              <motion.div className='image' key={image}> 
+                <img src={image} alt="" />
+              </motion.div>
+            )
+          })}
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
---
+++ src/Images.js
@@ -0,0 +1,11 @@
+import img1 from './assests/images/anim_2.jpg'
+import img2 from './assests/images/anim_3.jpg'
+import img3 from './assests/images/anim_5.jpg'
+import img4 from './assests/images/nature_1.jpg'
+import img5 from './assests/images/nature_6.jpg'
+import img6 from './assests/images/nature_7.jpg'
+import img7 from './assests/images/nature_8.jpg'
+import img8 from './assests/images/nature_9.jpg'
+import img9 from './assests/images/nature_10.jpg'
+
+export default [img1, img2, img3, img4, img5, img6, img7, img8, img9]
